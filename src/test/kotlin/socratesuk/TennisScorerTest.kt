0c1d61be668e897b3757c8b772e685e49b72cabc
romo <robert.moore@zuhlke.com>
2019-06-06 22:12:19 +0100

    start tcr^

--- src/test/kotlin/socratesuk/TennisScorerTest.kt
+++ src/test/kotlin/socratesuk/TennisScorerTest.kt
@@ -22,8 +22,8 @@ class TennisScorerTest {
     @Test
     fun `if player one scores twice, then player1 has 30 points`() {
         var tennisGame = TennisGame()
-        tennisGame = tennisGame.player1Scores()
-        tennisGame = tennisGame.player1Scores()
+                .player1Scores()
+                .player1Scores()
         assertThat(tennisGame, equalTo(TennisGame(player1Points = 30, player2Points = 0)))
     }
 
