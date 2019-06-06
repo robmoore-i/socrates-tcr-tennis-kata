628fbddfba4dc881d663988401417b4ed74259e8
romo <robert.moore@zuhlke.com>
2019-06-06 22:01:08 +0100

    start tcr^

--- src/test/kotlin/socratesuk/TennisScorerTest.kt
+++ src/test/kotlin/socratesuk/TennisScorerTest.kt
@@ -6,12 +6,14 @@ import org.junit.Test
 
 class TennisScorerTest {
     @Test
-    fun `game is initialised with player one with no points`() {
+    fun `game is initialised with players with no points`() {
         val tennisGame = TennisGame()
         assertThat(tennisGame.player1Points, equalTo(0))
+        assertThat(tennisGame.player2Points, equalTo(0))
     }
 }
 
 class TennisGame {
     val player1Points: Int = 0
+    val player2Points: Int = 0
 }
